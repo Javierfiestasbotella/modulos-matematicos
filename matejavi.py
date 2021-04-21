2fbd229d84441943330285ebdf592d9695d477b0
Your Name <javierfiestasbotella@gmail.com>
2021-04-21 15:24:14 +0200

    agregado multi_rusa como ultima uncio

--- matejavi.py
+++ matejavi.py
@@ -1,73 +1,69 @@
 
 
+
 def factorial(x):
   '''
-  Halla el factorial de un número, ose la multiplicación
-  de todos los numeros del 1 hasta ese número
-  >>> factorial(3)
-  6
-  >>> factorial(0)
-  1
-  >>> factorial(34)
-  295232799039604140847618609643520000000
-  >>> factorial(-3)
-  1
+    La función impar(n) devuelve:
+    -  True: si número es impar
+    - False: si número no es impar
+    >>> factorial(0)
+    1
+    >>> factorial(1)
+    1
+    >>> factorial(3)
+    6
+    >>> factorial(-4)
+    1
+
   '''
+
+
   factorial=1
   for i in range (1,x+1):
     factorial=factorial*i
-  print( factorial)
+  return factorial
 
 
 
 def potencia(a,b):
   '''
-  Eleva a al número b
-  >>> potencia(3,0)
-  1
+  La potencia eleva a a la potencia b
   >>> potencia(2,3)
   8
-
-
+  >>> potencia(6,0)
+  1
+  >>> potencia(0,4)
+  0
   '''
-
-
   potencia=a**b
   print(potencia)
 
 
 
 def sumatoria(a,b):
+  
   '''
-    Esta funcion suma todos los elementos que hay entre a y b 
-    éstos incluidos
-    >>> sumatoria(2,4)
-    9
-    >>> sumatoria(0,1)
-    1
-    >>> sumatoria(0,100)
-    5050
-
-
-
-     ''' 
+  >>> sumatoria(1,100)
+  5050
+  >>> sumatoria(-100,100)
+  0
+  
+  '''
   suma=0
+  '''
+  >>> suma(3,7)
+  10
+  >>> suma(-4,-6)
+  -10
+  >>> suma(-34,4)
+  -30
+  '''
   for i in range (a,b+1):
     suma=suma+i
   print(suma)
 
 
 def pitagoras(a,b,c): #coloca 0 el elemento para hallar
-  '''
-    Esta funcion halla el elemento que falta dentro 
-    del triángulo de pitágoras
-       
-    >>> pitagoras (0,5,3)  
-    la hipotenusa es igual a la raiz cuadrada de: 34 = 5.830951894845301
-
-
-
-     ''' 
   if a==0:
     a=a**2
     a=b**2+c**2
@@ -88,60 +84,71 @@ def pitagoras(a,b,c): #coloca 0 el elemento para hallar
 
 
 def mcd(a,b):# halla el mcd de dos numeros
-    listaA=[]
-    listaB=[]
-    for i in range(1,a+1):
-        if a%i==0:
-            listaA.append(i)
-    for j in range(1,b+1):
-        if b%j==0:
-            listaB.append(j)
-    lista_divisores=[]
-    for x in listaA:
-      for y in listaB:
-        if x==y:
-          lista_divisores.append(x)
+  '''
+  >>> mcd(3,12)
+  el mcd de los números  3 y 12= 3
+  >>> mcd (34,56)
+  el mcd de los números  34 y 56= 2
+  >>> mcd(27,144)
+  el mcd de los números  27 y 144= 9
+
+
+  
+  '''
+  listaA=[]
+  listaB=[]
+  for i in range(1,a+1):
+      if a%i==0:
+          listaA.append(i)
+  for j in range(1,b+1):
+      if b%j==0:
+          listaB.append(j)
+  lista_divisores=[]
+  for x in listaA:
+    for y in listaB:
+      if x==y:
+        lista_divisores.append(x)
     #for maximo in lista_divisores:
     maximo= max(lista_divisores, key=int)
-    print("el mcd de los nuemeros  " + str(a) +" y " + str(b) +"= "+ str(maximo))
+  print("el mcd de los números  " + str(a) +" y " + str(b) +"= "+ str(maximo))
 
 
 def tablas(numero):#crea la tabla de multiplicar de cualquier número
-  '''
-
- >>> tablas(2)
- 2 x 1 = 2
- 2 x 2 = 4
- 2 x 3 = 6
- 2 x 4 = 8
- 2 x 5 = 10
- 2 x 6 = 12
- 2 x 7 = 14
- 2 x 8 = 16
- 2 x 9 = 18
- 2 x 10 = 20
-
-
-  '''
   for i in range(1, 11):
     print(str(numero) + " x " + str(i) + " = " + str(numero * i))
 
+
+
+
 def porcentaje(a,b):#halla el tanto % de un numero. a=%, y b= al numero.
+  
 
   '''
   >>> porcentaje(30,10)
-  el 30%  de 10 = 3.0
+  el 30% de 10 = 3.0
+  >>> porcentaje(97,345)
+  el 97% de 345 = 334.65
+  >>> porcentaje(15,-100)
+  el 15% de -100 = -15.0
+
 
   '''
   x=(b*a)/100
-  print ("el " + str(a) + "%  de " + str(b) +" = " + str(x))
+  print ("el " + str(a) + "% de " + str(b) +" = " + str(x))
 
 def listado():
-  lista=("factorial(x), \npotencia(a,b), \nsumatoria(a,b), \npitagoras(a,b,c), \ntablas(numero), \nporcentaje(a,b), \nmcd(a,b),\nmcm(a,b),\necuacion(a,b), \nperfecto(a), \nbase10(a)","\nbinario(a)","\nnumeroabinario(a)")
+  lista=("factorial(x), \npotencia(a,b), \nsumatoria(a,b), \npitagoras(a,b,c), \ntablas(numero), \nporcentaje(a,b), \nmcd(a,b),\nmcm(a,b),\necuacion(a,b),  \necuacion2_grado(a,b,c), \nperfecto(a),  \nsumafraccion(a,b,c,d), \ndescomponer(x),\nfibonachi(m), \nmulti_rusa(x,y)")
   print(lista)
 
 
 def mcm(a,b):#funcion halla el mcm de dos numeros
+  '''
+  >>> mcm(4,12)
+  12
+  >>> mcm(3,4)
+  12
+  
+  '''
   divide2=0
   divide3=0
   divide5=0
@@ -185,7 +192,7 @@ def mcm(a,b):#funcion halla el mcm de dos numeros
   lista_a.append(l)
   lista_a.append(m)
 
-  #print(lista_a)
+  #print(lista_a
     
     
   while b>0:
@@ -245,24 +252,57 @@ def mcm(a,b):#funcion halla el mcm de dos numeros
   else:
     resultado=resultado*lista_b[4]
 
-  print("el mcm = "+str(resultado))
+  return resultado
+  #print("el mcm = "+str(resultado))
 
 
 def ecuacion(a,b):#ecuaciones de 1º grado ax+b=0
+  '''
+  >>> ecuacion(7,21)
+  -3.0
+  >>> ecuacion(6,3)
+  -0.5
+  
+  
+  '''
   
   if a==a and b==b:
-    print("x="+str(-b/a))
+    return -b/a
   elif a==-a and b==b:
-    print("x="+str(b/a))
+    return b/a
   elif a==a and b==-b:
-    print("x="+str(b/a))
+    return b/a
   elif a==-a and b==-b:
-      print("x="+str(-b/a))
+    return -b/a
   else:
     print("no se puede dividir entre 0")
- 
+
+def ecuacion_2grado(a,b,c):#halla los dos resultados + y - de ecuacion de 2ºgrado
+  '''
+  >>> ecuacion_2grado(1,3,2)
+  (-1.0, -2.0)
+  >>> ecuacion_2grado(1,-25,144)
+  (16.0, 9.0)
+  >>> ecuacion_2grado(1,-2,-80)
+  (10.0, -8.0)
+  
+  
+  '''
+  raiz=float((b**2-4*a*c))
+  raiz=raiz**0.5
+  x1=(-(b)+raiz)/2*a
+  x2=(-(b)-raiz)/2*a
+  return x1,x2
 
 def perfecto(a):#te dice si a es un numero perfecto o no.
+
+  '''
+  >>> perfecto (6)
+  True
+  >>> perfecto (46)
+  False
+  
+  '''
   perfect=[]
   for i in range (1,a):
     if a%i==0:
@@ -271,44 +311,97 @@ def perfecto(a):#te dice si a es un numero perfecto o no.
   for j in perfect:
     suma=suma+j
   if suma==a:
-    print(str(a)+" es un numero perfecto")
+    return True
   else:
-    print("lo siento "+str(a)+" no es un número perfecto")
+    return False
  
 
 
-def sumafraccion(x,b,c,d):
-  if x== a/b and y==c/d:
-    denominador=mcm(b,d) 
-    resultado=([a*(denominador/b)]+[c*(denominador/d)])
-  print(resultado)
-
-
-def base10(a):#te descompone cualquier numero en base 10
-  x=str(a)
-  base=len(x)-1
-  for i in x:
-    d=10**base
-    print(i +"x",end=" ")
-    print(d)
-    base=base-1
+def sumafraccion(a,b,c,d):#suma dos fracciones a/b + c/d
+  '''
+  >>> sumafraccion(5,3,5,4)
+  2.92
+  >>> sumafraccion(56,6,4,7)
+  34.67
+  '''
+  
+  while b!=0 or d!=0:
     
-def binario(a):# convierte cualquier numero binario a decimal
-  x=int(str(a),2)
-  print(x)
-      
-      
-def numeroabinario(a):# convierte un número entero a número binario
-  binlista=[]
-  while a>=1:
-    r=int(a%2)
-    binlista.append(r)
-    division=a/2
-    a=division
-  print()
-  for i in range(len(binlista)-1,-1,-1):
-    print(binlista[i],end="")
+    denominador=mcm(b,d)
+    resultado=(a*(denominador/b)+(c*(denominador/d)))/12
+    return float("{0:.2f}".format(resultado))
+  
 
-if __name__=='__main__':
+
+def descomponer(x):# descopone cualquier numero en base 10
+  '''
+  >>> descomponer(3011)
+  3 x 1000
+  0 x 100
+  1 x 10
+  1 x 1
+  '''
+  y=str(x)
+  base=len(y)
+  for numero in y:
+    if numero!="0":
+      print (numero+" x",10**(base-1))
+      base-=1
+    else:
+      print (numero+" x",10**(base-1))
+      base-=1
+      
+def fibonachi(m):#el atributo 'm' indica los primeros m numeros de la secuencia de fibonachi
+  '''
+  >>> fibonachi(7)
+  0
+  1
+  1
+  2
+  3
+  5
+  8
+   '''
+  f0=0 
+  f1=1
+  lista_fibo=[0,1]
+  for k in range (2, m):
+    f=f0+f1
+    f0=f1
+    f1=f
+    lista_fibo.append(f)
+  for i in lista_fibo:
+    print(i)
+
+
+def multi_rusa(x,y):#multiplicacion rusa
+  '''
+  >>> multi_rusa(25,6)
+  150
+  >>> multi_rusa(0,23)
+  0
+  '''
+  cont1=0
+  cont2=0
+  resultado=0
+  while y!=1:
+    y=y//2
+    cont1+=1
+  while cont2!=cont1:
+    x=x*2
+    resultado+=x
+    cont2+=1
+  return resultado
+
+
+
+
+
+
+
+
+listado ()  
+    
+if __name__=="__main__":
   import doctest
-  doctest.testmod() 
+  doctest.testmod()
